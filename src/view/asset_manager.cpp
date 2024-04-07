77f8c3d1e23ecfa8ac61401acc158c41839d9b85
Nikita Zolin <zolin.na@phystech.edu>
2024-04-07 17:53:31 +0300

    redesigned

--- src/view/asset_manager.cpp
+++ src/view/asset_manager.cpp
@@ -2,47 +2,43 @@
 
 AssetManager::AssetManager(SDL_Renderer* renderer)
         : renderer_(renderer), pieces_images_(2, std::vector<SDL_Texture*>(8)),
-            background_image_(IMG_LoadTexture(
-                renderer, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/game-background.png")),
-            board_image_(IMG_LoadTexture(
-                renderer, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/board.png")) {
+          background_image_(IMG_LoadTexture(
+                  renderer, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/game-background.png")),
+          board_image_(IMG_LoadTexture(
+                  renderer, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/board.png")) {
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Brown)] = IMG_LoadTexture(renderer_,
-                                                                                                            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/brown-1.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/brown-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Darkblue)] = IMG_LoadTexture(
-            renderer_,
-            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/darkblue-1.png");
+            renderer_, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/darkblue-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Green)] = IMG_LoadTexture(renderer_,
-                                                                                                            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/green-1.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/green-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Lightblue)] = IMG_LoadTexture(
-            renderer_,
-            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/lightblue-1.png");
+            renderer_, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/lightblue-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Orange)] = IMG_LoadTexture(renderer_,
-                                                                                                             "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/orange-1.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/orange-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Pink)] = IMG_LoadTexture(renderer_,
-                                                                                                           "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/pink-1.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/pink-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Red)] = IMG_LoadTexture(renderer_,
-                                                                                                          "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/red-1.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/red-1.png");
     pieces_images_[static_cast<int8_t>(Player::First)][static_cast<int8_t>(Color::Yellow)] = IMG_LoadTexture(renderer_,
-                                                                                                             "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1/yellow-1.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player1-v0/yellow-1.png");
 
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Brown)] = IMG_LoadTexture(renderer_,
-                                                                                                             "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/brown-2.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/brown-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Darkblue)] = IMG_LoadTexture(
-            renderer_,
-            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/darkblue-2.png");
+            renderer_, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/darkblue-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Green)] = IMG_LoadTexture(renderer_,
-                                                                                                             "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/green-2.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/green-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Lightblue)] = IMG_LoadTexture(
-            renderer_,
-            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/lightblue-2.png");
+            renderer_, "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/lightblue-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Orange)] = IMG_LoadTexture(renderer_,
-                                                                                                              "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/orange-2.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/orange-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Pink)] = IMG_LoadTexture(renderer_,
-                                                                                                            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/pink-2.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/pink-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Red)] = IMG_LoadTexture(renderer_,
-                                                                                                           "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/red-2.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/red-2.png");
     pieces_images_[static_cast<int8_t>(Player::Second)][static_cast<int8_t>(Color::Yellow)] = IMG_LoadTexture(renderer_,
-                                                                                                              "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2/yellow-2.png");
+            "/home/mrdelrus/Programming/MyProjects/Kamisado/assets/images/pieces/player2-v0/yellow-2.png");
 }
 
 SDL_Renderer* AssetManager::get_renderer() const {
